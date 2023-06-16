10d6a51545e28dd9ab5e07e26ecb252b9867d460
Oskar Flores <oskar@comalatech.com>
2023-06-16 17:48:45 +0200

    added basic terraform provider

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://tormenta.xmen.local:8006/api2/json"
+  pm_tls_insecure     = true
+  pm_api_token_id     = "root@pam!terraform_token_id"
+  pm_api_token_secret = var.proxmox_api_key
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,5 @@
+variable "proxmox_api_key" {
+  description = "Proxmox API key"
+  type        = string
+  sensitive   = true
+}
